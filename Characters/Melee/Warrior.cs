3872fb458dde772e85820723c6d538096ead7148
Luca Battesini <lucabattesini1@gmail.com>
2025-10-08 11:40:19 -0300

    Adding a name space in Warriors class

--- Characters/Melee/Warrior.cs
+++ Characters/Melee/Warrior.cs
@@ -1,25 +1,29 @@
-public class Warrior
-{ 
-    // Fields
-    private string? name;
-    private string? faction;
-    private int abilityPoints;
-    private int healthPoints;
-    private int level;
 
-    // Properties
+namespace OopGame.Characters.Melee
+{
+    public class Warrior
+    {
+        // Fields
+        private string? name;
+        private string? faction;
+        private int abilityPoints;
+        private int healthPoints;
+        private int level;
 
-    // Methods
-    public void Strike()
-    {
-        Console.WriteLine("Warrior is striking");
-    }
-    public void Execute()
-    {
-        Console.WriteLine("Warrior Executed");
-    }
-    public void SkinHarden()
-    {
-        Console.WriteLine("Warrior is skin harder");
+        // Properties
+
+        // Methods
+        public void Strike()
+        {
+            Console.WriteLine("Warrior is striking");
+        }
+        public void Execute()
+        {
+            Console.WriteLine("Warrior Executed");
+        }
+        public void SkinHarden()
+        {
+            Console.WriteLine("Warrior is skin harder");
+        }
     }
 }
