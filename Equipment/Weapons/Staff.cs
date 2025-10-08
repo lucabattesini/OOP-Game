131405877a97d3b6e52dc6e15f76a35c358de129
Luca Battesini <lucabattesini1@gmail.com>
2025-10-08 11:18:14 -0300

    Creating weapons classes

---
+++ Equipment/Weapons/Staff.cs
@@ -0,0 +1,13 @@
+public class Staff
+{
+    // Fields 
+    private int damage;
+
+    // Properties
+
+    // Methods
+    public void MagicAttack()
+    {
+        Console.WriteLine("Magic attack");
+    }
+}
---
+++ Equipment/Weapons/Sword.cs
@@ -0,0 +1,13 @@
+public class Sword
+{
+    // Fields
+    private int damage;
+
+    // Properties
+
+    // Methods
+    public void Attack()
+    {
+        Console.WriteLine("Attacked");
+    }
+}
